218fb30e09f4092ea4c9f630e671acbd59a42157
Brodown64 <59394822+Brodown64@users.noreply.github.com>
2025-11-03 19:29:37 -0600

    button logic

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #define FLAGS_DIR "flags/"
 
 const char* GetRandomFlagPath() {
-    static char filepath[256];
+    static char filepath[256]; 
     struct dirent *entry;
     DIR *dp = opendir(FLAGS_DIR);
 
@@ -16,9 +16,9 @@ const char* GetRandomFlagPath() {
         return NULL;
     }
 
-    const char* files[512];
+    const char* files[512]; // file amount detected
     int count = 0;
-    while ((entry = readdir(dp))!= NULL) {
+    while ((entry = readdir(dp))!= NULL) { // while directory is read
         if (strstr(entry->d_name, ".png")) {
             files[count++] = strdup(entry->d_name);
         }
@@ -48,6 +48,18 @@ int main(void) {
     bool showImage = false;
 
     Image country = LoadImage("flags/ar.png");
+    Texture2D button = LoadTexture("resources/button.jpg");
+
+    float frameHeight = (float)button.height;
+    Rectangle sourceRec = { 0, 0, (float)button.width, frameHeight };
+
+    Rectangle btnBounds = { screenWidth/2.0f - button.width/2.0f, screenHeight/2.0f - button.height/2.0f, (float)button.width, frameHeight };
+
+    Vector2 mousePoint = { 0.0f, 0.0f };
+
+    int btnState = 0;                
+    bool btnAction = false;         
+
 
     ImageResize(&country, 300, 200);
 
@@ -55,8 +67,11 @@ int main(void) {
 
     Vector2 position = {250, 200}; // position on screen
 
+    bool flagChosen = false;
+
     while (!WindowShouldClose()) {
-       if (IsKeyPressed(KEY_K)) {
+       mousePoint = GetMousePosition();
+       if (IsKeyPressed(KEY_K) && !flagChosen) {
             if (showImage) UnloadTexture(texture);
 
             const char* randomFlag = GetRandomFlagPath();
@@ -68,17 +83,47 @@ int main(void) {
             UnloadImage(flag);
 
             showImage = true;
+            flagChosen = true;
+
+        }
+
+        btnAction = false;
+
+        if (CheckCollisionPointRec(mousePoint, btnBounds))
+        {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                btnState = 2;
+            } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                btnAction = true;
+            } else {
+                btnState = 1;
+            }
+
+        } else {
+            btnState = 0;
+        }
+
+        if (btnAction)
+        {
+            printf("Works!");
+
+            // TODO: Any desired action
         }
 
         BeginDrawing();
         ClearBackground(darkGreen);
 
+        if (!flagChosen) {
+            DrawText("Press K to play", 340, 520, 20, RED);
+        } else {
+            DrawText("Only one flag per game!", 290, 520, 20, RED);
+        }
         if (showImage) {
             DrawTextureV(texture, position, WHITE);
+            DrawTextureRec(button, sourceRec, (Vector2){ btnBounds.x, btnBounds.y }, WHITE);
         }
 
         DrawText("Country Guess Game", 300, 80, 20, RED);
-        DrawText("Press K to play", 340, 520, 20, RED);
 
         EndDrawing();
     }
